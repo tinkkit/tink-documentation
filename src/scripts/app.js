dcdd0f546da4c3b350dee1183d203cd3b797e467
Jasper Van Proeyen <trianglejuice@mac.com>
2015-05-07 15:22:42 +0200

    First working version!

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -84,7 +84,7 @@
     languages:['html','js','bash','css']
   });
 }).run(function($rootScope) {
-  var versionFileTinkProp = '1.2.6';
+  var versionFileTinkProp = '1.0.2';
   $rootScope.themes = {
     digipolis:['tink.'+versionFileTinkProp+'.min.css','default.'+versionFileTinkProp+'.min.css','styleguide.'+versionFileTinkProp+'.min.css'],
     stad:['tink-stad.'+versionFileTinkProp+'.min.css','default-stad.'+versionFileTinkProp+'.min.css','styleguide-stad.'+versionFileTinkProp+'.min.css'],
