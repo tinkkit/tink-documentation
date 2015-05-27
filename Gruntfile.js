291f9966d0ce8c0f59d28ff1b4e89ebc8fc9a66e
Jasper Van Proeyen <trianglejuice@mac.com>
2015-05-27 14:35:05 +0200

    Changed grunt serve port for easier development

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,10 +107,10 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 9000,
+        port: 9002,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0',
-        livereload: 35729
+        livereload: 35731
       },
       livereload: {
         options: {
