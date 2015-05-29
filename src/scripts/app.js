078a4951ea59e7647e0a066faf936d7d26436d6d
Jasper Van Proeyen <trianglejuice@mac.com>
2015-05-29 11:22:39 +0200

    Some misc updates

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -87,7 +87,7 @@
     languages:['html','js','bash','css']
   });
 }).run(function($rootScope) {
-  var versionFileTinkProp = '1.1.0';
+  var versionFileTinkProp = '1.2.0';
   $rootScope.themes = {
     digipolis:['tink.'+versionFileTinkProp+'.min.css','default.'+versionFileTinkProp+'.min.css','styleguide.'+versionFileTinkProp+'.min.css'],
     stad:['tink-stad.'+versionFileTinkProp+'.min.css','default-stad.'+versionFileTinkProp+'.min.css','styleguide-stad.'+versionFileTinkProp+'.min.css'],
