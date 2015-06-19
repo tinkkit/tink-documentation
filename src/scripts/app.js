bb329a291e5e3e783fbfcb49224a02bce37c114b
Lieven VG <lievenvg@gmail.com>
2015-06-19 15:45:00 +0200

    Restyled color page and added theming

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -91,7 +91,7 @@
     languages:['html','js','bash','css']
   });
 }).run(function($rootScope) {
-  var versionFileTinkProp = '1.4.0';
+  var versionFileTinkProp = '1.4.1';
   $rootScope.themes = {
     digipolis:['tink.'+versionFileTinkProp+'.min.css','default.'+versionFileTinkProp+'.min.css','styleguide.'+versionFileTinkProp+'.min.css'],
     stad:['tink-stad.'+versionFileTinkProp+'.min.css','default-stad.'+versionFileTinkProp+'.min.css','styleguide-stad.'+versionFileTinkProp+'.min.css'],
