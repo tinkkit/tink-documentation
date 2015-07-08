1bc0c718f9561a3e8bb77b61711c6ab619b26c74
Vincent Bouillart <vincent.bouillart@appfoundry.be>
2015-07-08 10:05:18 +0200

    added tooltip documentation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -34,6 +34,7 @@
    'tink.interactivetable',
    'tink.identitycardnumber',
    'tink.sticktotop',
+   'tink.tooltip',
    'yaru22.md'
    ])
  .config(function ($routeProvider) { /*, $locationProvider */
