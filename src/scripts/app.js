ca8503a1b6d9fad0b8ef69d2997a36e2aa1d0620
Jasper Van Proeyen <trianglejuice@mac.com>
2015-08-12 10:32:28 +0200

    Upgraded docs to Angular 1.4.3

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -34,7 +34,8 @@
    'tink.interactivetable',
    'tink.identitycardnumber',
    'tink.sticktotop',
-   'tink.tooltip'
+   'tink.tooltip',
+   'yaru22.md'
    ])
  .config(function ($routeProvider) { /*, $locationProvider */
   $routeProvider
@@ -91,7 +92,7 @@
     languages:['html','js','bash','css']
   });
 }).run(function($rootScope) {
-  var versionFileTinkProp = '1.5.8';
+  var versionFileTinkProp = '1.5.10';
   $rootScope.themes = {
     digipolis:['tink.'+versionFileTinkProp+'.min.css','default.'+versionFileTinkProp+'.min.css','styleguide.'+versionFileTinkProp+'.min.css'],
     stad:['tink-stad.'+versionFileTinkProp+'.min.css','default-stad.'+versionFileTinkProp+'.min.css','styleguide-stad.'+versionFileTinkProp+'.min.css'],
