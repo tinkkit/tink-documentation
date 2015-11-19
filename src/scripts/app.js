4c58bd8c3830a60ea28d5e7636f023334e804f1c
Jasper Van Proeyen <trianglejuice@mac.com>
2015-11-19 09:19:01 +0100

    1.9.0 preparation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -87,7 +87,7 @@
     languages:['html','js','bash','css']
   });
 }).run(function($rootScope) {
-  var versionFileTinkProp = '1.8.0';
+  var versionFileTinkProp = '1.9.0';
   $rootScope.themes = {
     digipolis:['tink.'+versionFileTinkProp+'.min.css','default.'+versionFileTinkProp+'.min.css','styleguide.'+versionFileTinkProp+'.min.css'],
     stad:['tink-stad.'+versionFileTinkProp+'.min.css','default-stad.'+versionFileTinkProp+'.min.css','styleguide-stad.'+versionFileTinkProp+'.min.css'],
