d0de564182eed30bc5d0fe0f1a52ce14c18d2b51
Jasper Van Proeyen <trianglejuice@mac.com>
2016-02-03 11:57:42 +0100

    Merge

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -89,7 +89,7 @@
     languages:['html','js','bash','css']
   });
 }).run(function($rootScope) {
-  var versionFileTinkProp = '1.12.0';
+  var versionFileTinkProp = '1.12.1';
   $rootScope.themes = {
     digipolis:['tink.'+versionFileTinkProp+'.min.css','default.'+versionFileTinkProp+'.min.css','styleguide.'+versionFileTinkProp+'.min.css'],
     stad:['tink-stad.'+versionFileTinkProp+'.min.css','default-stad.'+versionFileTinkProp+'.min.css','styleguide-stad.'+versionFileTinkProp+'.min.css'],
