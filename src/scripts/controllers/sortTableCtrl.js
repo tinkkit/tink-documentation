8f61c03c23935994f8740f1ed4100454ffa28690
Jasper Van Proeyen <trianglejuice@mac.com>
2016-03-22 10:24:32 +0100

    Corrected a bug in the sort table example + updated the code

--- src/scripts/controllers/sortTableCtrl.js
+++ src/scripts/controllers/sortTableCtrl.js
@@ -1,20 +1,17 @@
 'use strict';
 angular.module('tinkApp')
-.controller('controller',function($scope){
+.controller('sortTableCtrl',function($scope){
     var ctrl = this;
+
   ctrl.rapporten = {};
+
   $scope.sorted = function(property,order,type){
-    console.log('sorted',property,order,type);
+    // console.log('sorted',property,order,type);
   };
   var index = 0;
+
   $scope.loadData = function(){
-    ctrl.rapporten = {data:[{volgnummer:new Date(),familienaam:'Bouillart',voornaam:'Vincent'},{volgnummer:new Date(),familienaam:'De Mulder',voornaam:'Kevin'},{volgnummer:new Date(),familienaam:'Van Proeyen',voornaam:'Jasper'},{volgnummer:new Date(),familienaam:'Wuyts',voornaam:'Tom'},{volgnummer:102544,familienaam:'Hermans',voornaam:'Tom'},{volgnummer:102544,familienaam:'Van Gestel',voornaam:'Lieven'}]};
-
-    if(index ===1){
-      ctrl.rapporten = {data:[{volgnummer:new Date(),familienaam:'Bouillart',voornaam:'Vincent'},{volgnummer:new Date(),familienaam:'De Mulder',voornaam:'Kevin'},{volgnummer:new Date(),familienaam:'Van Proeyen',voornaam:'Jasper'}]};
-    }
-  index++;
+    ctrl.rapporten = {data:[{datum:new Date(),familienaam:'Bouillart',voornaam:'Vincent'},{datum:new Date(),familienaam:'De Mulder',voornaam:'Kevin'},{datum:new Date(),familienaam:'Van Proeyen',voornaam:'Jasper'},{datum:new Date(),familienaam:'Wuyts',voornaam:'Tom'},{datum:102544,familienaam:'Hermans',voornaam:'Tom'},{datum:102544,familienaam:'Van Gestel',voornaam:'Lieven'}]};
   };
-
 
 });
